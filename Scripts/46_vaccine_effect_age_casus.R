231dfd24ca0ca822d36d9d463517967a2c967039
Yorick <62314888+YorickBleijenberg@users.noreply.github.com>
2021-02-27 22:26:44 +0100

    Add files via upload

---
+++ Scripts/46_vaccine_effect_age_casus.R
@@ -0,0 +1,89 @@
+
+n= 149
+
+new.date <- Sys.Date()-n
+old.date <- new.date-1
+
+
+old.casus.file <-paste("C:\\Rdir\\rivm-data\\COVID-19_casus_landelijk_", old.date, ".csv", sep = "")
+old.casus <- read.csv(old.casus.file,sep=";")
+old.casus$Date_statistics <- as.Date(old.casus$Date_statistics)
+
+new.casus.file <-paste("C:\\Rdir\\rivm-data\\COVID-19_casus_landelijk_", new.date, ".csv", sep = "")
+new.casus <- read.csv(new.casus.file,sep=";")
+new.casus$Date_statistics <- as.Date(new.casus$Date_statistics)
+
+old.casus.age <- casus.working<-count(old.casus,Agegroup)
+new.casus.age <- casus.working<-count(new.casus,Agegroup)
+
+colnames(new.casus.age) <- c("Agegroup","n2")
+
+cassus.age.merge <- merge(old.casus.age,new.casus.age)
+
+cassus.age.merge$diff <- cassus.age.merge$n2-cassus.age.merge$n
+
+sum =  sum(cassus.age.merge$diff)
+cassus.age.merge.short <- cassus.age.merge[ -c(2,3)]
+
+date.text = as.character(new.date)
+cassus.age.merge.fin <- rbind(c("date",date.text), cassus.age.merge.short)
+
+cassus.age.spread.base <- spread(cassus.age.merge.fin, Agegroup, diff)
+
+
+
+
+n= 148
+
+
+while (n > 0){
+
+new.date <- Sys.Date()-n
+old.date <- new.date-1
+
+
+old.casus.file <-paste("C:\\Rdir\\rivm-data\\COVID-19_casus_landelijk_", old.date, ".csv", sep = "")
+old.casus <- read.csv(old.casus.file,sep=";")
+old.casus$Date_statistics <- as.Date(old.casus$Date_statistics)
+
+new.casus.file <-paste("C:\\Rdir\\rivm-data\\COVID-19_casus_landelijk_", new.date, ".csv", sep = "")
+new.casus <- read.csv(new.casus.file,sep=";")
+new.casus$Date_statistics <- as.Date(new.casus$Date_statistics)
+
+old.casus.age <- casus.working<-count(old.casus,Agegroup)
+new.casus.age <- casus.working<-count(new.casus,Agegroup)
+
+colnames(new.casus.age) <- c("Agegroup","n2")
+
+cassus.age.merge <- merge(old.casus.age,new.casus.age)
+
+cassus.age.merge$diff <- cassus.age.merge$n2-cassus.age.merge$n
+
+sum =  sum(cassus.age.merge$diff)
+cassus.age.merge.short <- cassus.age.merge[ -c(2,3)]
+
+date.text = as.character(new.date)
+cassus.age.merge.fin <- rbind(c("date",date.text), cassus.age.merge.short)
+
+cassus.age.spread <- spread(cassus.age.merge.fin, Agegroup, diff)
+
+cassus.age.spread.base <- rbind(cassus.age.spread.base, cassus.age.spread)
+
+
+n <- n-1
+
+}
+ 
+
+
+cassus.age.spread.base
+
+
+File_date_5 <- paste0("data/_casus_daily_age_dif.csv")
+
+write.csv2(cassus.age.spread.base, File_date_5, row.names=FALSE)
+
+
+
+
+
